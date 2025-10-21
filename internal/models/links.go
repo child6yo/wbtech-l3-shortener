658c2f5eb244095bd47380e8df4efbe335dbd2e8
Арсений Глушков <arseniyglsh@gmail.com>
2025-10-21 21:17:23 +0700

    feat: link model

--- internal/models/links.go
+++ internal/models/links.go
@@ -1,1 +1,14 @@
 package models
+
+// ShortLink сокращенная ссылка на ресурс, кастомная или генерируемая сервисом.
+// Существует для редиректа на FullLink.
+type ShortLink string
+
+// FullLink полная ссылка, ведующая непосредственно на ресурс.
+type FullLink string
+
+// Link представляет модель ссылок.
+type Link struct {
+	Short ShortLink
+	Full  FullLink
+}
